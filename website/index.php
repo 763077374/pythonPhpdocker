8eb9e6c30c1d205ca2e2f5504a19553d6435cd31
763077374@qq.com <763077374@qq.com>
2018-02-01 17:57:59 +0000

    php explain python web return json

---
+++ website/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Shop</title>
+</head>
+<body>
+<h1>Welcome to my shop</h1>
+<ul>
+    <?php
+         $json = file_get_contents('http://product-service');
+         $obj = json_decode($json);
+         $products = $obj->products;
+        foreach ($products as $product){
+            echo "<li>$product</li>";
+        }
+
+    ?>
+
+</ul>
+</body>
+</html>
